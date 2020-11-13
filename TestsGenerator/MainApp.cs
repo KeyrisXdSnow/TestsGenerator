ec92135864ba9486c0ea1db90d0c6697166d3448
Keyris <KeyrisXdSnow>
2020-11-13 16:32:54 +0300

    defines the specification of the pipeline

--- TestsGenerator/MainApp.cs
+++ TestsGenerator/MainApp.cs
@@ -8,13 +8,16 @@ namespace TestsGenerator
         public static void Main(string[] args)
         {
             Stack<string> stack = new Stack<string>() ;
-
-            for (int i = 0; i < 10; i++)
-            {
-                stack.Push(i.ToString());
-            }
-            FileReader reader = new FileReader(5);
-            reader.LoadClasses(stack);
+            
+            stack.Push("E:\\Sharaga\\SPP\\TestsGenerator\\TestsGenerator\\Classes\\ClassFormatter.cs");
+            stack.Push("E:\\Sharaga\\SPP\\TestsGenerator\\TestsGenerator\\Classes\\ConstructorFormatter.cs");
+            stack.Push("E:\\Sharaga\\SPP\\TestsGenerator\\TestsGenerator\\Classes\\FieldFormatter.cs");
+            stack.Push("E:\\Sharaga\\SPP\\TestsGenerator\\TestsGenerator\\Classes\\MethodFormatter.cs");
+            stack.Push("E:\\Sharaga\\SPP\\TestsGenerator\\TestsGenerator\\Classes\\PropertiesFormatter.cs");
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     stack.Push(i.ToString());
+            // }
             
 
 
---
+++ TestsGeneratorLib/TestsGenerator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace TestsGeneratorLib
+{
+    public class TestsGenerator
+    {
+        private readonly int _readFileThreadsAmount;
+        private readonly int _processedTasksThreadsAmount;
+        private readonly int _writeFileThreadsAmount;
+
+        public TestsGenerator(int readFileThreadsAmount, int processedTasksThreadsAmount, int writeFileThreadsAmount)
+        {
+            _readFileThreadsAmount = readFileThreadsAmount;
+            _processedTasksThreadsAmount = processedTasksThreadsAmount;
+            _writeFileThreadsAmount = writeFileThreadsAmount;
+        }
+
+        public Task GenerateCLasses()
+        {
+            var maxFilesToLoad = new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = _readFileThreadsAmount };
+            var maxTestToGenerate = new ExecutionDataflowBlockOptions() {MaxDegreeOfParallelism = _processedTasksThreadsAmount};
+            var maxFilesToWrite = new ExecutionDataflowBlockOptions() {MaxDegreeOfParallelism = _writeFileThreadsAmount};
+
+            // Create a dataflow block that takes a  path as input and returns a file text
+            var loadClasses = new TransformBlock<string, string> (GetTextFromFile,maxFilesToLoad); // add lambda and try/catch
+            
+            // Create a dataflow block that takes a file text and return generated Tests
+            var generateTests = new TransformBlock<string, string[]>(GetTestFromText, maxTestToGenerate); // add lambda and try/catch
+            
+            // Create a dataflow block that save generated Test on disk 
+            var writeTests = new ActionBlock<string[]>(WriteTests, maxFilesToLoad); // add lambda and try/catch
+            
+            //
+            // Connect the dataflow blocks to form a pipeline.
+            // 
+            var linkOption = new DataflowLinkOptions
+            {
+                PropagateCompletion = true
+            };
+            loadClasses.LinkTo(generateTests, linkOption);
+            generateTests.LinkTo(writeTests, linkOption);
+            
+            //// вот здесь непросредственно цикл переборов путей
+            
+            // Mark the head of the pipeline as complete.
+            loadClasses.Completion.Wait();
+
+            return writeTests.Completion;
+        }
+
+        private static async Task<string> GetTextFromFile(string path)
+        {
+            using var sourceStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read,
+                bufferSize: 4096, useAsync: true);
+
+            var sb = new StringBuilder();
+
+            var buffer = new byte[0x1000];
+            int numRead;
+            
+            while ((numRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+            {
+                var text = Encoding.Unicode.GetString(buffer, 0, numRead);
+                sb.Append(text);
+            }
+
+            return sb.ToString();
+        }
+
+        private static async Task<string[]> GetTestFromText(string text)
+        {
+            return null;
+        }
+
+        private static async Task WriteTests(string[] tests)
+        {
+            return;
+        }
+        
+    }
+    
+}
